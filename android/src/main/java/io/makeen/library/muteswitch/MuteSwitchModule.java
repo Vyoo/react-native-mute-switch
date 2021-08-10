044136eca4e0c3046111e844a0b0fbf69169dbeb
Oleg Filimonov <themraction@gmail.com>
2021-08-10 23:26:12 +0300

    feat: Add android mute switch callback

--- android/src/main/java/io/makeen/library/muteswitch/MuteSwitchModule.java
+++ android/src/main/java/io/makeen/library/muteswitch/MuteSwitchModule.java
@@ -2,18 +2,57 @@
 
 package io.makeen.library.muteswitch;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class MuteSwitchModule extends ReactContextBaseJavaModule {
 
+    private AudioManager am;
+
     private final ReactApplicationContext reactContext;
+
+    private String status = "unknown";
 
     public MuteSwitchModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+    }
+
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        this.am = (AudioManager) reactContext.getSystemService(Context.AUDIO_SERVICE);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (am.getRingerMode()) {
+                    case AudioManager.RINGER_MODE_SILENT:
+                        updateStatus("silent");
+                        break;
+                    case AudioManager.RINGER_MODE_VIBRATE:
+                        updateStatus("vibrate");
+                        break;
+                    case AudioManager.RINGER_MODE_NORMAL:
+                        updateStatus("normal");
+                        break;
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
+        reactContext.registerReceiver(receiver, filter);
     }
 
     @Override
@@ -21,9 +60,17 @@ public class MuteSwitchModule extends ReactContextBaseJavaModule {
         return "MuteSwitch";
     }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    private void updateStatus(String status) {
+        if (this.status.equals(status)) {
+            return;
+        }
+        this.status = status;
+
+        WritableMap params = Arguments.createMap();
+        params.putString("status", status);
+
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("MuteSwitch_change", params);
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // main index.js
 
-import { NativeModules } from 'react-native';
+import {NativeModules, DeviceEventEmitter, NativeEventEmitter} from 'react-native';
+const {MuteSwitch} = NativeModules;
+const emitter = new NativeEventEmitter(NativeModules.MuteSwitch);
 
-const { MuteSwitch } = NativeModules;
+const subscribe = (callback) => {
+  const listener = emitter.addListener('MuteSwitch_change', callback);
+  return listener.remove;
+}
 
+export {subscribe};
 export default MuteSwitch;
